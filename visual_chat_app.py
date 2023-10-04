7c22493e3fe529f2418e36ef2994a8209b94cabc
hajarahm <hajarahnantege@gmail.com>
2023-10-04 22:23:15 +0400

    updated visual chat

--- visual_chat_app.py
+++ visual_chat_app.py
@@ -11,6 +11,7 @@ from langchain.vectorstores import FAISS
 from langchain.llms import CTransformers
 from langchain.chains import ConversationalRetrievalChain
 from pygwalker.api.streamlit import init_streamlit_comm, get_streamlit_html
+from transformers import AutoModelForCausalLM
 
 DB_FAISS_PATH = 'vectorstore/db_faiss'
 
@@ -29,18 +30,7 @@ class InteractiveVisualization:
                         dataset_names.append(f"{category}/{dataset_name}")
         return dataset_names
 
-    def visualize_dataset(self):
-        st.title("Interactive Dataset Visualization")
-        dataset_names = self.list_datasets()
-
-        if not dataset_names:
-            st.warning("No preprocessed datasets found in the 'preprocessed_data' folder.")
-            return
-
-        selected_dataset = st.selectbox("Select a dataset to visualize", dataset_names)
-        selected_dataset_path = os.path.join(self.preprocessed_data_folder, selected_dataset.replace('/', os.sep) + '.csv')
-
-        col1, col2 = st.columns(2)
+    def visualize_dataset(self, selected_dataset, selected_dataset_path, col1, col2):
 
         if col1.button("Visualize"):
             st.write(f"Visualizing dataset: {selected_dataset}")
@@ -66,66 +56,52 @@ class InteractiveVisualization:
             category, dataset_name = selected_dataset.split("/")
             self.view_html_report(category, dataset_name)
 
-    def chat_with_dataset(self, uploaded_file):
-        st.title("Interactive Dataset Visualization")
-        dataset_names = self.list_datasets()
+    def chat_with_dataset(self, selected_dataset):
 
-        if not dataset_names:
-            st.warning("No preprocessed datasets found in the 'preprocessed_data' folder.")
-            return
+        loader = CSVLoader(file_path=selected_dataset, encoding="utf-8", csv_args={'delimiter': ','})
+        data = loader.load()
 
-        selected_dataset = st.selectbox("Select a dataset to visualize", dataset_names)
-        selected_dataset_path = os.path.join(self.preprocessed_data_folder, selected_dataset.replace('/', os.sep) + '.csv')
+        embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2', model_kwargs={'device': 'cpu'})
 
-        if uploaded_file:
-            with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-                tmp_file.write(uploaded_file.getvalue())
-                tmp_file_path = tmp_file.name
+        db = FAISS.from_documents(data, embeddings)
+        db.save_local(DB_FAISS_PATH)
 
-            loader = CSVLoader(file_path=tmp_file_path, encoding="utf-8", csv_args={'delimiter': ','})
-            data = loader.load()
+        llm = self.load_llm()
 
-            embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2', model_kwargs={'device': 'cpu'})
+        chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=db.as_retriever())
 
-            db = FAISS.from_documents(data, embeddings)
-            db.save_local(DB_FAISS_PATH)
+        def conversational_chat(query):
+            result = chain({"question": query, "chat_history": st.session_state['history']})
+            st.session_state['history'].append((query, result["answer"]))
+            return result["answer"]
 
-            llm = self.load_llm()
+        if 'history' not in st.session_state:
+            st.session_state['history'] = []
 
-            chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=db.as_retriever())
+        if 'generated' not in st.session_state:
+            st.session_state['generated'] = ["Hello ! Ask me about " + selected_dataset.name + " 🤗"]
 
-            def conversational_chat(query):
-                result = chain({"question": query, "chat_history": st.session_state['history']})
-                st.session_state['history'].append((query, result["answer"]))
-                return result["answer"]
+        if 'past' not in st.session_state:
+            st.session_state['past'] = ["Hey ! 👋"]
 
-            if 'history' not in st.session_state:
-                st.session_state['history'] = []
+        response_container = st.container()
+        container = st.container()
 
-            if 'generated' not in st.session_state:
-                st.session_state['generated'] = ["Hello ! Ask me(LLAMA2) about " + uploaded_file.name + " 🤗"]
+        with container:
+            with st.form(key='my_form', clear_on_submit=True):
+                user_input = st.text_input("Query:", placeholder="Talk to csv data 👉 (:", key='input')
+                submit_button = st.form_submit_button(label='Send')
 
-            if 'past' not in st.session_state:
-                st.session_state['past'] = ["Hey ! 👋"]
+            if submit_button and user_input:
+                output = conversational_chat(user_input)
+                st.session_state['past'].append(user_input)
+                st.session_state['generated'].append(output)
 
-            response_container = st.container()
-            container = st.container()
-
-            with container:
-                with st.form(key='my_form', clear_on_submit=True):
-                    user_input = st.text_input("Query:", placeholder="Talk to csv data 👉 (:", key='input')
-                    submit_button = st.form_submit_button(label='Send')
-
-                if submit_button and user_input:
-                    output = conversational_chat(user_input)
-                    st.session_state['past'].append(user_input)
-                    st.session_state['generated'].append(output)
-
-            if st.session_state['generated']:
-                with response_container:
-                    for i in range(len(st.session_state['generated'])):
-                        message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="big-smile")
-                        message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
+        if st.session_state['generated']:
+            with response_container:
+                for i in range(len(st.session_state['generated'])):
+                    message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="big-smile")
+                    message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
 
     def view_html_report(self, category, dataset_name):
         html_report_path = os.path.join(
@@ -143,8 +119,8 @@ class InteractiveVisualization:
 
     def load_llm(self):
         llm = CTransformers(
-            model="llama-2-7b-chat.ggmlv3.q8_0.bin",
-            model_type="llama",
+            model_path="/Applications/gpt4all/gpt4all-training/chat/",
+            model="gpt4all-lora-quantized.bin",
             max_new_tokens=512,
             temperature=0.5
         )
@@ -153,6 +129,7 @@ class InteractiveVisualization:
     def run_app(self):
         st.title("Interactive Dataset Visualization")
         dataset_names = self.list_datasets()
+        col1, col2, col3 = st.columns(3)
 
         if not dataset_names:
             st.warning("No preprocessed datasets found in the 'preprocessed_data' folder.")
@@ -161,11 +138,11 @@ class InteractiveVisualization:
         selected_dataset = st.selectbox("Select a dataset to visualize", dataset_names)
         selected_dataset_path = os.path.join(self.preprocessed_data_folder, selected_dataset.replace('/', os.sep) + '.csv')
 
-        self.visualize_dataset()
+        self.visualize_dataset(selected_dataset, selected_dataset_path, col1, col2)
 
-        if st.button("Chat with Dataset"):
+        if col3.button("Chat with Dataset"):
             st.write(f"Chatting with dataset: {selected_dataset}")
-            self.chat_with_dataset(selected_dataset)
+            self.chat_with_dataset(selected_dataset_path)
 
 if __name__ == "__main__":
     visualization = InteractiveVisualization()
